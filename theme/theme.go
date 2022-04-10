fd926de2ea6df8354c54df0852d69aeea52364d2
Matthew Irving <matthew.irving@cse-cst.gc.ca>
2022-04-09 22:49:02 -0400

    ✨ theme package to define console themes

--- theme/theme.go
+++ theme/theme.go
@@ -1,6 +1,47 @@
 package theme
 
-const (
-	InputRequest = "\033[34m" // color.Blue
-	ErrorMessage = "\033[31m" // color.Red
+import (
+	"fmt"
+	"github.com/TwiN/go-color"
 )
+
+const (
+	reset        = "\033[0m"
+	bold         = "\033[1m"
+	red          = "\033[31m"
+	green        = "\033[32m"
+	yellow       = "\033[33m"
+	blue         = "\033[34m"
+	purple       = "\033[35m"
+	cyan         = "\033[36m"
+	gray         = "\033[37m"
+	white        = "\033[97m"
+	InputRequest = blue
+	ErrorMessage = red
+
+	RepositoryTitle = green
+	PullState       = green
+	PullTitle       = gray
+	PullUrl         = blue
+	UserLogin       = yellow
+	DebugMessage    = gray
+	LogLevel        = yellow
+)
+
+const debug = false
+
+func ColorString(messageColor string, message string) (msg string) {
+	msg = color.Ize(messageColor, fmt.Sprintf(message))
+	return
+}
+
+func PrintColorString(color string, message string) {
+	fmt.Print(ColorString(color, message))
+}
+
+// PrintDebug TODO: Move to some logging package
+func PrintDebug(message string) {
+	if debug {
+		PrintColorString(color.Ize(yellow, "[debug] ")+DebugMessage, message)
+	}
+}
